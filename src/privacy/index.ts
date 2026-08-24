c5081c0db338e7ab22d8a9cb63e001e79ababc43
Hibiki49aaiii <86287098+Hibiki49aaiii@users.noreply.github.com>
2026-08-24 22:53:51 +0900

    feat: export RAILGUN private-state reader

--- src/privacy/index.ts
+++ src/privacy/index.ts
@@ -1,4 +1,5 @@
 export * from "./railgun.js";
 export * from "./railgun-adapter.js";
 export * from "./railgun-broadcaster.js";
+export * from "./railgun-private-state.js";
 export * from "./verification.js";
