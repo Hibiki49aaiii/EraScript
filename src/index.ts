2a9e2813bf437a789c38325fe0dbe8b7e4d2ce85
Hibiki49aaiii <86287098+Hibiki49aaiii@users.noreply.github.com>
2026-08-24 17:54:39 +0900

    feat: expose privacy protocol overlays

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export * from "./diagnostics.js";
 export * from "./transform.js";
 export * from "./typecheck.js";
 export * from "./chains/index.js";
+export * from "./privacy/index.js";
 export * from "./web3/index.js";
