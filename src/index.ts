87f7b759255310168da9a8e04102afd74f9a3669
Hibiki49aaiii <86287098+Hibiki49aaiii@users.noreply.github.com>
2026-08-24 17:51:03 +0900

    feat: expose multichain chain-family APIs

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export * from "./compiler.js";
 export * from "./diagnostics.js";
 export * from "./transform.js";
 export * from "./typecheck.js";
+export * from "./chains/index.js";
 export * from "./web3/index.js";
