b98fdea7f8175451cc0357e23fe3a440997f0522
Hibiki49aaiii <86287098+Hibiki49aaiii@users.noreply.github.com>
2026-08-29 00:48:38 +0900

    feat: export RAILGUN Wallet SDK private-state bridge

--- src/privacy/index.ts
+++ src/privacy/index.ts
@@ -2,4 +2,5 @@ export * from "./railgun.js";
 export * from "./railgun-adapter.js";
 export * from "./railgun-broadcaster.js";
 export * from "./railgun-private-state.js";
+export * from "./railgun-wallet-sdk-private-state.js";
 export * from "./verification.js";
